f4a99c8aa5c03914cb77fe8537e5cc6174a20830
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-11-08 23:25:14 +0530

    Remove stickler errors

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <div className='App'>App barebones</div>;
+  return <div className="App">App barebones</div>;
 }
 
 export default App;
