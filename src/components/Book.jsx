be23cc7272dde1d87008a6bd95e6766b7dc516b5
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-11-10 23:22:38 +0530

    Fix more stickler errors

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Book = ({ book: { id, title, category } }) => {
-  return (
-    <tr>
-      <td>{id}</td>
-      <td>{title}</td>
-      <td>{category}</td>
-    </tr>
-  );
-};
+const Book = ({ book: { id, title, category } }) => (
+  <tr>
+    <td>{id}</td>
+    <td>{title}</td>
+    <td>{category}</td>
+  </tr>
+);
 
 Book.propTypes = {
-  book: PropTypes.instanceOf(Object),
+  book: PropTypes.instanceOf(Object).isRequired,
 };
 
 export default Book;
--- src/containers/BooksList.jsx
+++ src/containers/BooksList.jsx
@@ -4,27 +4,25 @@ import PropTypes from 'prop-types';
 
 import Book from '../components/Book';
 
-const BooksList = ({ books }) => {
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Book ID</th>
-          <th>Title</th>
-          <th>category</th>
-        </tr>
-      </thead>
-      <tbody>
-        {books.map(book => (
-          <Book key={book.id} book={book} />
-        ))}
-      </tbody>
-    </table>
-  );
-};
+const BooksList = ({ books }) => (
+  <table>
+    <thead>
+      <tr>
+        <th>Book ID</th>
+        <th>Title</th>
+        <th>category</th>
+      </tr>
+    </thead>
+    <tbody>
+      {books.map(book => (
+        <Book key={book.id} book={book} />
+      ))}
+    </tbody>
+  </table>
+);
 
 BooksList.propTypes = {
-  books: PropTypes.instanceOf(Array),
+  books: PropTypes.instanceOf(Array).isRequired,
 };
 
 const mapStateToProps = state => ({
