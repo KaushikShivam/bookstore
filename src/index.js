0588250037d80b629d50dec9ad296bf57f0daea9
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-11-10 21:38:58 +0530

    Add store to the index

--- src/index.js
+++ src/index.js
@@ -1,34 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+
 import './index.css';
 import App from './components/App';
 
-const STATE = [
-  {
-    id: 1,
-    title: 'Book 1',
-    category: 'Action',
-  },
-  {
-    id: 2,
-    title: 'Book 2',
-    category: 'Biography',
-  },
-  {
-    id: 3,
-    title: 'Book 3',
-    category: 'History',
-  },
-  {
-    id: 4,
-    title: 'Book 4',
-    category: 'Horror',
-  },
-  {
-    id: 5,
-    title: 'Book 5',
-    category: 'Kids',
-  },
-];
+import rootReducer from './reducers/index';
+const middlewares = [logger];
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const STATE = {
+  books: [
+    {
+      id: 1,
+      title: 'Book 1',
+      category: 'Action',
+    },
+    {
+      id: 2,
+      title: 'Book 2',
+      category: 'Biography',
+    },
+    {
+      id: 3,
+      title: 'Book 3',
+      category: 'History',
+    },
+    {
+      id: 4,
+      title: 'Book 4',
+      category: 'Horror',
+    },
+    {
+      id: 5,
+      title: 'Book 5',
+      category: 'Kids',
+    },
+  ],
+};
+
+const store = createStore(rootReducer, STATE, applyMiddleware(...middlewares));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
