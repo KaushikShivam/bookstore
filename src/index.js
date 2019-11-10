9147eac9b9a28d0284b2d9d3de8c8059ff3f955c
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-11-10 21:31:25 +0530

    Add initial state

--- src/index.js
+++ src/index.js
@@ -3,4 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 
+const STATE = [
+  {
+    id: 1,
+    title: 'Book 1',
+    category: 'Action',
+  },
+  {
+    id: 2,
+    title: 'Book 2',
+    category: 'Biography',
+  },
+  {
+    id: 3,
+    title: 'Book 3',
+    category: 'History',
+  },
+  {
+    id: 4,
+    title: 'Book 4',
+    category: 'Horror',
+  },
+  {
+    id: 5,
+    title: 'Book 5',
+    category: 'Kids',
+  },
+];
+
 ReactDOM.render(<App />, document.getElementById('root'));
