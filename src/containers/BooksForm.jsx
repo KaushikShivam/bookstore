3b203a031b8e117515bb9e344f6ac02d87600b42
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-11-10 19:19:55 +0530

    Add container comps

---
+++ src/containers/BooksForm.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class BooksForm extends Component {
+  render() {
+    return <div className="BooksForm">Books Form empty</div>;
+  }
+}
+
+export default BooksForm;
---
+++ src/containers/BooksList.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class BooksList extends Component {
+  render() {
+    return <div className="BooksList">Books List empty component</div>;
+  }
+}
+
+export default BooksList;
