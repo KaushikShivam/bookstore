40cbdcceb287a8c6106deba5936bb8191bd394b8
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-11-10 20:25:34 +0530

    Implement BookList comp with redux connect

--- src/containers/BooksList.jsx
+++ src/containers/BooksList.jsx
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import Book from '../components/Book';
 
 class BooksList extends Component {
   render() {
-    return <div className="BooksList">Books List empty component</div>;
+    const { books } = this.props;
+    return (
+      <table>
+        <tr>
+          <th>Book ID</th>
+          <th>Title</th>
+          <th>category</th>
+        </tr>
+        {books.map(book => (
+          <Book book={book} />
+        ))}
+      </table>
+    );
   }
 }
 
-export default BooksList;
+const mapStateToProps = state => ({
+  books: state.books,
+});
+
+export default connect(
+  mapStateToProps,
+  null,
+)(BooksList);
