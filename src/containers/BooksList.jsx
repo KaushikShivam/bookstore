fa59b1acd61f7eed0dccf5d3b50490ba427d8553
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-11-10 21:41:09 +0530

    Refactor table

--- src/containers/BooksList.jsx
+++ src/containers/BooksList.jsx
@@ -8,14 +8,18 @@ class BooksList extends Component {
     const { books } = this.props;
     return (
       <table>
-        <tr>
-          <th>Book ID</th>
-          <th>Title</th>
-          <th>category</th>
-        </tr>
-        {books.map(book => (
-          <Book book={book} />
-        ))}
+        <thead>
+          <tr>
+            <th>Book ID</th>
+            <th>Title</th>
+            <th>category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {books.map(book => (
+            <Book id={book.id} book={book} />
+          ))}
+        </tbody>
       </table>
     );
   }
