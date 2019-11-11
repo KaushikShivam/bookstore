d227066f054c9cb074e610ec9f0d4b2411baf585
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-11-11 16:48:27 +0530

    Add filter label

--- src/components/CategoryFilter.jsx
+++ src/components/CategoryFilter.jsx
@@ -4,13 +4,16 @@ const CategoryFilter = () => {
   const bookCategories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
   const mergedFilters = ['All', ...bookCategories];
   return (
-    <select>
-      {mergedFilters.map((val, idx) => (
-        <option key={idx} value={val}>
-          {val}
-        </option>
-      ))}
-    </select>
+    <div>
+      Filter:
+      <select>
+        {mergedFilters.map((val, idx) => (
+          <option key={idx} value={val}>
+            {val}
+          </option>
+        ))}
+      </select>
+    </div>
   );
 };
 
