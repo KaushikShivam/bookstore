c09f66e5c335f24f396abcda94ea3e96c87151c4
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-11-11 17:16:32 +0530

    Refactor filter to be class comp

--- src/components/CategoryFilter.jsx
+++ src/components/CategoryFilter.jsx
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-const CategoryFilter = () => {
-  const bookCategories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
-  const mergedFilters = ['All', ...bookCategories];
-  return (
-    <div>
-      Filter:
-      <select>
-        {mergedFilters.map((val, idx) => (
-          <option key={idx} value={val}>
-            {val}
-          </option>
-        ))}
-      </select>
-    </div>
-  );
-};
+class CategoryFilter extends Component {
+  render() {
+    const bookCategories = [
+      'Action',
+      'Biography',
+      'History',
+      'Horror',
+      'Kids',
+      'Learning',
+      'Sci-Fi',
+    ];
+    const mergedFilters = ['All', ...bookCategories];
+    return (
+      <div>
+        Filter:
+        <select>
+          {mergedFilters.map((val, idx) => (
+            <option key={idx} value={val}>
+              {val}
+            </option>
+          ))}
+        </select>
+      </div>
+    );
+  }
+}
+
+CategoryFilter.propTypes = {};
 
 export default CategoryFilter;
