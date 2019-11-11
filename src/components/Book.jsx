a884040605d0c1af2e464b05b80d101518daf14a
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-11-11 21:23:11 +0530

    Add basic Book styles

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Book.scss';
+import loader from '../assets/loader.png';
 
-const Book = ({ book: { id, title, category }, removeBook }) => (
-  <tr>
-    <td>{id}</td>
-    <td>{title}</td>
-    <td>{category}</td>
-    <td>
-      <button type="submit" onClick={removeBook}>
-        Remove Book
-      </button>
-    </td>
-  </tr>
+const Book = ({ book: { title, category }, removeBook }) => (
+  <div className="Book">
+    <div>
+      <p className="Book-category">{category}</p>
+      <h4 className="Book-title">{title}</h4>
+      <p className="Book-author">Author Name</p>
+      <div className="Book-buttons">
+        <button className="Book-ctc" type="submit">
+          Comments
+        </button>
+        <button className="Book-ctc" type="submit" onClick={removeBook}>
+          Remove
+        </button>
+        <button className="Book-ctc" type="submit">
+          Edit
+        </button>
+      </div>
+    </div>
+  </div>
 );
 
 Book.propTypes = {
